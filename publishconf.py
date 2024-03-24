902a686fc6b4480f2ac623ca43570f630ab27a54
callmenezha <783627014@qq.com>
2024-03-24 22:57:13 +0800

    update: siteurl to github pages url

--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://cside-ce.github.io'
+SITEURL = 'https://connetinfotech.github.io/cside-ce/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
